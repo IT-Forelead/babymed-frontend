91fb75a5e0ff6fda08a7846eeaeed079650ab1f9
OG'ABEK <shomoyxhacker007@mail.ru>
2022-11-17 11:44:07 +0500

    services section done

--- src/components/Service/ServiceItem.vue
+++ src/components/Service/ServiceItem.vue
@@ -1,17 +1,49 @@
 <script setup>
 import 'v3-infinite-loading/lib/style.css'
-import { toRefs } from 'vue'
-import TrashIcon from '../../assets/icons/TrashIcon.vue';
-import EditIcon from '../../assets/icons/EditIcon.vue';
+import { ref, toRefs, watch } from 'vue'
+import TrashIcon from '../../assets/icons/TrashIcon.vue'
+import EditIcon from '../../assets/icons/EditIcon.vue'
 import useMoneyFormatter from '../../mixins/currencyFormatter.js'
+import ServicesService from '../../services/services.service'
+import notify from 'izitoast'
+import 'izitoast/dist/css/iziToast.min.css'
+import { useServicesStore } from '../../store/services.store'
+import { useModalStore } from '../../store/modal.store'
 
 const props = defineProps({
   services: { type: Array, required: true },
 })
 
 const { services } = toRefs(props)
+const selectedServiceId = ref('')
 
+const deleteService = (s_id) => {
+  selectedServiceId.value = s_id
+  useModalStore().openDeleteAlert()
+}
 
+watch(
+  () => useModalStore().confirmDelete,
+  (confirm) => {
+    if (confirm) {
+      ServicesService.deleteService(selectedServiceId.value)
+        .then(() => {
+          notify.success({
+            message: 'Service successfully deleted!',
+          })
+          ServicesService.getAllServices().then((res) => {
+            useServicesStore().setServices(res)
+          })
+          selectedServiceId.value = ''
+        })
+        .catch(() => {
+          notify.warning({
+            message: 'Error while deleting service!',
+          })
+        })
+    }
+  }
+)
 </script>
 <template>
   <tr class="border-y border-gray-200 hover:bg-gray-100 text-lg font-medium" v-for="(service, idx) in services" :key="idx">
@@ -24,10 +56,10 @@ const { services } = toRefs(props)
     </td>
     <td v-motion-pop class="py-3 px-6 text-center">
       <div class="flex item-center justify-center">
-        <div class="w-4 mr-3 transform text-blue-500 hover:text-purple-500 hover:scale-110 cursor-pointer">
+        <!-- <div class="w-4 mr-3 transform text-blue-500 hover:text-purple-500 hover:scale-110 cursor-pointer">
           <EditIcon class="w-6 h-6" />
-        </div>
-        <div class="w-4 mr-3 transform text-red-500 hover:text-red-600 hover:scale-110 cursor-pointer">
+        </div> -->
+        <div @click="deleteService(service?.id)" class="w-4 mr-3 transform text-red-500 hover:text-red-600 hover:scale-110 cursor-pointer">
           <TrashIcon class="w-6 h-6" />
         </div>
       </div>
