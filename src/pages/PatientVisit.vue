991fcf264c2e08979cdeba695149e5582452170d
OG'ABEK <shomoyxhacker007@mail.ru>
2022-11-21 12:40:32 +0500

    payment report

--- src/pages/PatientVisit.vue
+++ src/pages/PatientVisit.vue
@@ -7,6 +7,7 @@ import AddVisit from '../components/AddVisit.vue'
 import { useModalStore } from '../store/modal.store'
 import { onMounted, watch } from 'vue'
 import { useDropStore } from '../store/drop.store'
+import useMoneyFormatter from '../mixins/currencyFormatter'
 
 const API_URL = import.meta.env.VITE_BASE_URL
 
@@ -94,7 +95,45 @@ watch(
         </div>
         <AddVisit />
       </div>
-      <div v-if="Object.keys(selectedDoctor).length !== 0 || Object.keys(selectedPatient).length !== 0 || Object.keys(selectedService).length !== 0" class="bg-white rounded-lg p-3 col-span-2">{{ selectedPatient }} {{ selectedDoctor }} {{ selectedService }}</div>
+      <div v-if="Object.keys(selectedDoctor).length !== 0 || Object.keys(selectedPatient).length !== 0 || Object.keys(selectedService).length !== 0" class="bg-white rounded-lg p-3 col-span-2 text-gray-600">
+        <h1 class="text-2xl font-bold">{{ $t('patient') }}</h1>
+        <div class="border-y-2 border-dashed my-2 p-3">
+          <div class="flex justify-between border-b-2 border-dotted py-2">
+            <div class="font-medium text-base">{{ $t('patient') }}:</div>
+            <div class="text-right text-lg font-semibold text-gray-700">
+              <div class="capitalize" v-if="Object.keys(selectedPatient).length !== 0">
+                {{ selectedPatient?.patient?.firstname + ' ' + selectedPatient?.patient?.lastname }}
+              </div>
+              <div v-else class="capitalize">********</div>
+              <div v-if="Object.keys(selectedPatient).length !== 0">{{ selectedPatient?.patient?.phone }}</div>
+              <div v-if="Object.keys(selectedPatient).length !== 0">{{ selectedPatient?.region?.name + ' ' + selectedPatient?.town?.name }}</div>
+              <div class="capitalize" v-if="Object.keys(selectedPatient).length !== 0">{{ selectedPatient?.patient?.address }}</div>
+            </div>
+          </div>
+          <div class="flex justify-between border-b-2 border-dotted py-2">
+            <div class="font-bold">{{ $t('doctor') }}:</div>
+            <div class="capitalize text-right text-lg font-semibold text-gray-700" v-if="Object.keys(selectedDoctor).length !== 0">
+              {{ selectedDoctor?.firstname + ' ' + selectedDoctor?.lastname }} <br />
+              {{ selectedDoctor?.phone }}
+            </div>
+            <div v-else class="capitalize">********</div>
+          </div>
+          <div class="flex justify-between py-2">
+            <div class="font-bold">{{ $t('service') }}:</div>
+            <div class="capitalize text-right text-lg font-semibold text-gray-700" v-if="Object.keys(selectedService).length !== 0">
+              {{ selectedService?.name }} <br />
+              {{ useMoneyFormatter(selectedService?.cost) }}
+            </div>
+            <div v-else class="capitalize">********</div>
+          </div>
+        </div>
+        <div v-if="Object.keys(selectedService).length !== 0" class="flex items-center justify-between font-bold text-2xl px-3">
+          <div>{{ $t('total') }}:</div>
+          <div class="text-right font-semibold text-gray-700">
+            {{ useMoneyFormatter(selectedService?.cost) }}
+          </div>
+        </div>
+      </div>
       <div v-else class="max-h-[82vh] overflow-auto patients-wrapper bg-white rounded-lg col-span-2">
         <div class="flex items-center justify-between p-3">
           <p class="text-3xl font-bold">Visits Report</p>
