15904d5d4a7a84ba199f0881a67edc8d40c1137c
OG'ABEK <shomoyxhacker007@mail.ru>
2022-12-05 15:09:42 +0500

    403 Forbidden fix

--- src/views/Visits.vue
+++ src/views/Visits.vue
@@ -29,6 +29,17 @@ const loadPatients = async ($state) => {
         }),
         headers: authHeader(),
       })
+      if (response?.headers.get('x-new-token')) {
+        sessionStorage.setItem('token', response?.headers.get('x-new-token'))
+        await fetch(`${API_URL}/visit/report`, {
+          method: 'POST',
+          body: JSON.stringify({
+            page: 1,
+            limit: 10,
+          }),
+          headers: authHeader(),
+        })
+      }
       const json = await response.json()
       total.value = json?.total
       setTimeout(() => {
