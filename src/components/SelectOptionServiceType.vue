06ace1752764f52ccb7d7e4d53f046863338c867
Surojiddin <isurojiddin@gmail.com>
2022-12-09 16:32:15 +0500

    select inputs changed

--- src/components/SelectOptionServiceType.vue
+++ src/components/SelectOptionServiceType.vue
@@ -39,11 +39,8 @@ const optionClicked = (data) => {
 <template>
   <div class="select-none">
     <label ref="dropdown" class="flex items-center w-full relative">
-      <div @click="useDropStore().openServiceTypeDropDown()" class="w-11 h-11 flex items-center rounded-l-lg justify-center bg-gray-100 cursor-pointer">
-        <UserBoldIcon class="w-10 h-10 border-r rounded-lg p-2" />
-      </div>
-      <div v-if="selectedOption" class="border-none focus:ring-0 outline-0 bg-gray-100 w-full text-lg rounded-r-lg pl-2 py-2 capitalize" v-text="selectedOption?.name"></div>
-      <div @click="useDropStore().openServiceTypeDropDown()" v-else class="border-none bg-gray-100 py-2 w-full text-lg rounded-r-lg cursor-pointer text-gray-500 pl-2">{{ $t('select') }}</div>
+      <div v-if="selectedOption" class="border-none focus:ring-0 outline-0 bg-gray-100 w-full text-lg rounded-lg pl-2 py-2 capitalize" v-text="selectedOption?.name"></div>
+      <div @click="useDropStore().openServiceTypeDropDown()" v-else class="border-none bg-gray-100 py-2 w-full text-lg rounded-lg cursor-pointer text-gray-500 pl-2">{{ $t('select') }}</div>
       <ChevronRightIcon @click="useDropStore().openServiceTypeDropDown()" v-if="!selectedOption" class="absolute right-2.5 z-10 rotate-90 cursor-pointer text-gray-600" />
       <TimesIcon @click="clearSelectedOptionData()" v-if="selectedOption" class="absolute right-2.5 z-10 cursor-pointer bg-gray-500 hover:bg-gray-600 text-white rounded-full p-1" />
       <div v-if="useDropStore().isOpenServiceTypeDropDown" class="absolute shadow p-2 z-20 top-12 max-h-56 overflow-auto w-full bg-gray-100 rounded-lg divide-y">
