83ea80c37bf733a4cd6099de95fdf965d5a4ac4b
Surojiddin <isurojiddin@gmail.com>
2022-12-23 17:17:38 +0500

    time format changed

--- src/components/ReportItem.vue
+++ src/components/ReportItem.vue
@@ -36,7 +36,7 @@ const addVisitThisPatient = (patient) => {
       </div>
     </td>
     <td v-motion-pop class="py-3 px-6 text-left">{{ patient?.patient?.phone }}</td>
-    <td v-motion-pop class="py-3 px-6 text-center">{{ moment(patient?.patient?.createdAt).format('MM/DD/YYYY h:mm:ss') }}</td>
+    <td v-motion-pop class="py-3 px-6 text-center">{{ moment(patient?.patient?.createdAt).format('MM/DD/YYYY h:mm') }}</td>
     <td v-motion-pop class="py-3 px-6 text-center text-sm">
       {{ patient?.region?.name }} <br />
       {{ patient?.city?.name }} <br />
