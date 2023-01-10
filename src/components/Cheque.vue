6900ed892c1b39d0bc6905c9c83a19b047f4baab
Navruz <safabayev@mail.ru>
2023-01-10 15:43:15 +0500

    Cheque Item Updates

--- src/components/Cheque.vue
+++ src/components/Cheque.vue
@@ -1,26 +1,29 @@
 <script setup>
-import {computed} from '@vue/reactivity'
+import { computed } from '@vue/reactivity'
 import useMoneyFormatter from '../mixins/currencyFormatter'
-import {useModalStore} from '../store/modal.store'
+import { useModalStore } from '../store/modal.store'
 import print from 'print-js'
 import moment from 'moment'
-import {useDropStore} from '../store/drop.store'
-import {useI18n} from 'vue-i18n'
+import { useDropStore } from '../store/drop.store'
+import { useI18n } from 'vue-i18n'
 
-const {t} = useI18n()
+const { t } = useI18n()
 
 const printing = () => {
   print({
     printable: 'ticket',
     type: 'html',
     showModal: true,
-    font_size: '16px',
+    font_size: '15px',
     modalMessage: t('preparingCheck'),
     style: `
     * {
       margin: 0;
       padding: 0;
-      font-family: "Lucida Console";
+      // font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+      // font-family: Georgia, 'Times New Roman', Times, serif;
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      // font-family: "Lucida Console", "Courier New", monospace;
     }
     #ticket {
       text-align: center;
@@ -54,56 +57,63 @@ const totalPrice = () => {
 </script>
 <template>
   <div v-if="useModalStore().isOpenPrintModal"
-       class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 backdrop-blur bg-gray-900/75 w-full max-h-screen md:inset-0 md:h-full">
-    <div class="relative p-4 w-full h-full max-w-xs md:h-auto left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+    class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 backdrop-blur bg-gray-900/75 w-full max-h-screen md:inset-0 md:h-full">
+    <div class="relative p-4 w-full h-full max-w-sm md:h-auto left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
       <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
         <div class="p-1 flex justify-center" id="ticket">
-          <div>
+          <div class="text-center">
             <div class="flex items-center justify-center space-x-3 mb-2 pb-2 header">
-              <img src="/cheque_logo.jpg" class="w-[80%]" alt="Logo"/>
+              <img src="/cheque_logo.jpg" class="w-[80%]" alt="Logo" />
             </div>
             <center>
-              <p>Многопрофильный медицинский центр</p>
+              <p>Куп тармокли тиббиёт маркази</p>
             </center>
             <hr class="margin-y">
             <p class="text-center checkid mx-3">Check No: {{ report?.patientVisit?.id }} </p>
-            <h1 class="text-center font-bold checkid">
-              {{ report?.patient?.firstname + ' ' + report?.patient?.lastname }}
+            <h1 class="text-center checkid">
+              -------------------- <br>
+               {{ report?.patient?.firstname + ' ' + report?.patient?.lastname }} <br>
+               {{ report?.patient?.phone}} <br>
+              --------------------
             </h1>
-              <h1>ВНИМАНИЕ!!!</h1>
-              <p>Порядок приёма пациентов:</p>
-              <span>Сначала проходят пациенты заранее записанные на определённое время. 
-                Пациенты ожидающие в живой очереди, будут проходить на приём, после заранее записанных пациентов.
-                ПАЦИЕНТОВ НА СКОРОЙ ПОМОЩИ ПРИНИМАЕМ БЕЗ ОЧЕРЕДИ!!!</span>
+            <h1>ДИККАТ!!!</h1>
+            <span>Беморларни кабул килиш тартиби: биринчи навбатда олдиндан малум вактга навбат олган беморлар, улардан
+              кейин жонли
+              навбатда турганлар киради.
+              Тез ёрдамда келган беморлар ва тугрук дарди билан келган аёллар шифокор кабулига НАВБАТСИЗ
+              киради!!!</span>
             <hr class="margin-y">
-            <table class="w-full max-w-[255px] mx-auto">
+            <table class="w-full mx-auto text-left">
               <tr class="text-sm" v-for="(service, idx) in report.services" :key="idx">
                 <td>{{ idx + 1 }}.</td>
                 <td class="capitalize">
-                  <p>{{ service?.name }}</p>
-                  <strong>{{ useMoneyFormatter(service?.price) }}</strong>
+                  {{ service?.serviceTypeName }} - {{ service?.name }}
+                  <br>
+                  {{ useMoneyFormatter(service?.price) }}
                 </td>
               </tr>
             </table>
             <hr class="margin-y">
-            <p class="text-center px-3 total text-lg">Итоговая цена:</p>
-            <h1 class="text-center px-3 font-bold total text-xl total-price">{{ useMoneyFormatter(totalPrice()) }}</h1>
-            <p class="text-center px-3 total">Зарегистрировал(а): {{
-                report?.userFirstName + ' ' + report?.userLastName
-              }}</p>
-            <p class="text-center px-3 total">Дата: {{
-                moment(report?.patientVisit?.createdAt).format('DD/MM/YYYY h:mm')
-              }}</p>
+            <p class="text-center px-3 total text-lg">Умумий нарх:</p>
+            <h1 class="text-center px-3 total total-price">
+              *** {{ useMoneyFormatter(totalPrice()) }} ***
+            </h1>
+            <p class="text-center px-3 total">Руйхатга олди: {{
+              report?.userFirstName + ' ' + report?.userLastName
+            }}</p>
+            <p class="text-center px-3 total">Сана: {{
+              moment(report?.patientVisit?.createdAt).format('DD/MM/YYYY h:mm')
+            }}</p>
           </div>
         </div>
         <div class="p-3 flex justify-end space-x-2">
           <div @click="closeModal()" class="bg-gray-500 rounded p-1 px-4 text-white cursor-pointer">{{
-              $t('close')
-            }}
+            $t('close')
+          }}
           </div>
           <div @click="printing()" class="bg-green-500 rounded p-1 px-4 text-white cursor-pointer">{{
-              $t('print')
-            }}
+            $t('print')
+          }}
           </div>
         </div>
       </div>
