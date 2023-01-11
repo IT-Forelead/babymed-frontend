bc8c45bb91fd0a65389551696ca02ed9e06de3e2
Surojiddin <isurojiddin@gmail.com>
2023-01-11 15:38:23 +0500

    remove getAllVisitsForExpense funtionality

--- src/services/visit.service.js
+++ src/services/visit.service.js
@@ -11,9 +11,6 @@ class VisitService {
   async getAllVisits(filter) {
     return AxiosService.post('/visit/report?page=1&limit=1000', filter, { headers: authHeader() })
   }
-  async getAllVisitsForExpense() {
-    return AxiosService.get('/visit/report/712852a6-830f-45bd-9797-43c5c50ba1df', { headers: authHeader() })
-  }
   async changePaymentStatus(id) {
     return AxiosService.get(`/visit/update-payment-status/${id}`, { headers: authHeader() })
   }
