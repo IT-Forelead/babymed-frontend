fd486ebc5992d90ea4060bf062c8ebc261e99ed0
Surojiddin <isurojiddin@gmail.com>
2023-01-18 15:05:52 +0500

    multi select style improved

--- src/components/MultiSelect.vue
+++ src/components/MultiSelect.vue
@@ -23,7 +23,7 @@ onClickOutside(multiselect, () => {
 
 <template>
   <div class="relative" ref="multiselect">
-    <div class="absolute z-10 overflow-y-auto bg-gray-100 border w-full mt-2 p-1 rounded-lg divide-y shadow" :id="id">
+    <div class="absolute z-10 overflow-y-auto bg-gray-100 border w-full mt-2 p-1 max-h-56 overflow-auto rounded-lg divide-y shadow" :id="id">
       <div v-for="(option, idx) in options" :key="idx" @click="store.setSelectService(option)">
         <div class="flex items-center justify-between p-1 rounded cursor-pointer hover:bg-slate-200">
           {{ option?.name }}
