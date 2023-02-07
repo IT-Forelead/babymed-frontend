ab527984068ae738aab0e7ac113d627ce8e5d78d
Surojiddin <isurojiddin@gmail.com>
2023-02-07 18:18:33 +0500

    apex chart added

--- src/components/Main/Statistics/BigCard.vue
+++ src/components/Main/Statistics/BigCard.vue
@@ -1,28 +1,86 @@
-<script setup></script>
+<script setup>
+import VisitService from '../../../services/visit.service'
+import { onMounted } from 'vue'
+import { useVisitStore } from '../../../store/visit.store'
+import moment from 'moment'
+
+const series = [
+  {
+    name: 'Tasgriflar soni',
+    data: [121, 32, 70, 126, 116, 121, 113],
+  },
+]
+
+const chartOptions = {
+  chart: {
+    height: 350,
+    type: 'bar',
+    events: {
+      click: function (chart, w, e) {
+        // console.log(chart, w, e)
+      },
+    },
+  },
+  colors: ['#A3E635', '#A3E635', '#111827'],
+  plotOptions: {
+    bar: {
+      borderRadius: 5,
+      columnWidth: '45%',
+      distributed: true,
+      dataLabels: {
+        position: 'top', // top, center, bottom
+      },
+    },
+  },
+  dataLabels: {
+    enabled: true,
+    formatter: function (val) {
+      return val
+    },
+    offsetY: -20,
+    style: {
+      fontSize: '14px',
+      colors: ['#304758'],
+    },
+  },
+  legend: {
+    show: false,
+  },
+  xaxis: {
+    categories: ['Dushanba', 'Seshanba', 'Choranba', 'Payanba', 'Juma', 'Shanba', 'Yakshanba'],
+    labels: {
+      style: {
+        fontSize: '12px',
+      },
+    },
+    tooltip: {
+      enabled: true,
+    },
+  },
+}
+
+onMounted(() => {
+  VisitService.getVisits({
+    startDate: moment().startOf('isoWeek').format().toString().slice(0, 19),
+    endDate: moment().endOf('isoWeek').format().toString().slice(0, 19),
+  }).then((res) => {
+    useVisitStore().clearStore()
+    useVisitStore().setPatients(res?.data)
+  })
+})
+</script>
 <template>
-  <div class="bg-white rounded-lg w-full p-5">
-    <h1 class="text-3xl font-bold">Heart Rate</h1>
-    <div class="flex items-center justify-between">
-      <p class="font-medium text-lg">Heart Candition</p>
-      <select class="border-none rounded-lg bg-gray-100 capitalize text-gray-400">
-        <option value="" selected>Every month</option>
-        <option value="1">Test</option>
-      </select>
+  <div class="bg-white rounded-lg w-full">
+    <div class="flex items-center justify-between p-5">
+      <div>
+        <h1 class="text-3xl font-bold">Haftalik tashriflar</h1>
+        <p class="font-medium text-lg">Har bir kungi statistikasi</p>
+      </div>
+      <div class="flex items-end space-x-2">
+        <div class="text-4xl font-bold">817</div>
+        <div class="text-xl font-medium text-gray-700">ta</div>
+      </div>
     </div>
-    <div class="flex items-center space-x-4">
-      <p class="text-4xl font-bold">30%</p>
-      <div class="rounded-lg bg-lime-300 px-1">+4.2%</div>
-    </div>
-    <p class="text-center">Chart Below</p>
-    <div class="flex items-baseline space-x-10 justify-center">
-      <div class="h-14 w-10 rounded bg-gray-100"></div>
-      <div class="h-24 w-10 rounded bg-gray-100"></div>
-      <div class="h-32 w-10 rounded bg-gray-100"></div>
-      <div class="h-44 w-10 rounded bg-gray-100"></div>
-      <div class="h-56 w-10 rounded bg-gray-100"></div>
-      <div class="h-32 w-10 rounded bg-gray-100"></div>
-      <div class="h-44 w-10 rounded bg-gray-100"></div>
-      <div class="h-56 w-10 rounded bg-gray-100"></div>
-    </div>
+    <apexchart type="bar" height="320" :options="chartOptions" :series="series"></apexchart>
   </div>
 </template>
--- src/components/Main/Statistics/StatisticsSection.vue
+++ src/components/Main/Statistics/StatisticsSection.vue
@@ -1,18 +1,129 @@
 <script setup>
 import BigCard from './BigCard.vue'
 import PaymentIcon from '../../../assets/icons/PaymentIcon.vue'
-import RecentPatientOutlineIcon from '../../../assets/icons/RecentPatientOutlineIcon.vue';
-import MedicalDoctorIcon from '../../../assets/icons/MedicalDoctorIcon.vue';
+import RecentPatientOutlineIcon from '../../../assets/icons/RecentPatientOutlineIcon.vue'
+import MedicalDoctorIcon from '../../../assets/icons/MedicalDoctorIcon.vue'
 import { useI18n } from 'vue-i18n'
-import { onMounted } from 'vue';
-import PatientService from '../../../services/patient.service';
+import { onMounted } from 'vue'
+import PatientService from '../../../services/patient.service'
 import { ref } from '@vue/reactivity'
-import UserService from '../../../services/user.service';
+import UserService from '../../../services/user.service'
 
 const { t } = useI18n()
 
 const patients = ref(0)
 const doctors = ref(0)
+
+const series = [
+  {
+    name: 'Tasgriflar soni',
+    data: [121, 32, 70, 126, 116, 121, 113],
+  },
+]
+
+const chartOptions = {
+  chart: {
+    height: 350,
+    type: 'bar',
+    events: {
+      click: function (chart, w, e) {
+        // console.log(chart, w, e)
+      },
+    },
+  },
+  colors: ['#FFF', '#111827', '#FFF', '#FFF'],
+  plotOptions: {
+    bar: {
+      borderRadius: 5,
+      columnWidth: '45%',
+      distributed: true,
+      dataLabels: {
+        position: 'top', // top, center, bottom
+      },
+    },
+  },
+  dataLabels: {
+    enabled: true,
+    formatter: function (val) {
+      return val
+    },
+    offsetY: -17,
+    style: {
+      fontSize: '12px',
+      colors: ['#304758'],
+    },
+  },
+  legend: {
+    show: false,
+  },
+  xaxis: {
+    categories: ['Du', 'Se', 'Cho', 'Pa', 'Ju', 'Sha', 'Yak'],
+    labels: {
+      style: {
+        fontSize: '12px',
+      },
+    },
+    tooltip: {
+      enabled: true,
+    },
+  },
+}
+
+// second chart
+const series2 = [
+  {
+    name: 'Tasgriflar soni',
+    data: [13421, 3342, 7430, 12634, 11634, 12341, 11343],
+  },
+]
+
+const chartOptions2 = {
+  chart: {
+    height: 350,
+    type: 'bar',
+    events: {
+      click: function (chart, w, e) {
+        // console.log(chart, w, e)
+      },
+    },
+  },
+  colors: ['#A3E635', '#111827', '#A3E635', '#A3E635'],
+  plotOptions: {
+    bar: {
+      borderRadius: 5,
+      columnWidth: '45%',
+      distributed: true,
+      dataLabels: {
+        position: 'top', // top, center, bottom
+      },
+    },
+  },
+  dataLabels: {
+    enabled: true,
+    formatter: function (val) {
+      return val
+    },
+    offsetY: -17,
+    style: {
+      fontSize: '12px',
+      colors: ['#304758'],
+    },
+  },
+  legend: {
+    show: false,
+  },
+  xaxis: {
+    categories: ['Du', 'Se', 'Cho', 'Pa', 'Ju', 'Sha', 'Yak'],
+    labels: {
+      style: {
+        fontSize: '12px',
+      },
+    },
+    tooltip: {
+      enabled: true,
+    },
+  },
+}
 
 onMounted(() => {
   PatientService.getAllPatients({}).then((res) => {
@@ -30,8 +141,8 @@ onMounted(() => {
     <BigCard />
     <div class="space-y-5">
       <div class="grid grid-cols-2 gap-5">
-        <div class="bg-lime-300 rounded-lg w-full p-5">
-          <div class="flex justify-between mb-3">
+        <div class="bg-lime-300 rounded-lg w-full">
+          <div class="flex justify-between p-5">
             <div>
               <p>Bill Due</p>
               <p class="text-2xl font-bold">1,8M+ UZS</p>
@@ -40,7 +151,10 @@ onMounted(() => {
               <PaymentIcon class="w-7 h-7" />
             </div>
           </div>
-          <div class="flex items-baseline space-x-3">
+          <div class="px-1">
+            <apexchart type="bar" height="180" :options="chartOptions" :series="series"></apexchart>
+          </div>
+          <!-- <div class="flex items-baseline space-x-3">
             <div class="h-14 w-7 rounded bg-gray-100"></div>
             <div class="h-24 w-7 rounded bg-gray-900"></div>
             <div class="h-28 w-7 rounded bg-gray-100"></div>
@@ -48,10 +162,10 @@ onMounted(() => {
             <div class="h-24 w-7 rounded bg-gray-100"></div>
             <div class="h-32 w-7 rounded bg-gray-900"></div>
             <div class="h-14 w-7 rounded bg-gray-100"></div>
-          </div>
+          </div> -->
         </div>
-        <div class="bg-white rounded-lg w-full p-5">
-          <div class="flex justify-between mb-3">
+        <div class="bg-white rounded-lg w-full">
+          <div class="flex justify-between p-5">
             <div>
               <p>Bill Due</p>
               <p class="text-2xl font-bold">1,8M+ UZS</p>
@@ -60,7 +174,10 @@ onMounted(() => {
               <PaymentIcon class="w-7 h-7" />
             </div>
           </div>
-          <div class="flex items-baseline space-x-3">
+          <div class="px-1">
+            <apexchart type="bar" height="180" :options="chartOptions2" :series="series2"></apexchart>
+          </div>
+          <!-- <div class="flex items-baseline space-x-3">
             <div class="h-14 w-7 rounded bg-lime-300"></div>
             <div class="h-24 w-7 rounded bg-gray-900"></div>
             <div class="h-28 w-7 rounded bg-lime-300"></div>
@@ -68,11 +185,11 @@ onMounted(() => {
             <div class="h-24 w-7 rounded bg-lime-300"></div>
             <div class="h-32 w-7 rounded bg-gray-900"></div>
             <div class="h-14 w-7 rounded bg-lime-300"></div>
-          </div>
+          </div> -->
         </div>
       </div>
       <div class="grid grid-cols-2 gap-5">
-        <div class="bg-white rounded-lg w-full p-5 space-y-7">
+        <div class="bg-white rounded-lg w-full p-5 space-y-2">
           <div class="flex justify-between mb-3">
             <div>
               <p>Bemorlar</p>
@@ -84,7 +201,7 @@ onMounted(() => {
           </div>
           <p>{{ $t('numberOfRegisteredPatients') }}</p>
         </div>
-        <div class="bg-white rounded-lg w-full p-5 space-y-7">
+        <div class="bg-white rounded-lg w-full p-5 space-y-2">
           <div class="flex justify-between mb-3">
             <div>
               <p>{{ $t('doctors') }}</p>
