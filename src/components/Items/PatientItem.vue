d5482f5001faef6bf81cee0de1999551154902a5
Surojiddin <isurojiddin@gmail.com>
2023-03-01 17:32:25 +0500

    small changes

--- src/components/Items/PatientItem.vue
+++ src/components/Items/PatientItem.vue
@@ -1,24 +1,29 @@
 <script setup>
-import EditIcon from '../../assets/icons/EditIcon.vue'
 import TrashIcon from '../../assets/icons/TrashIcon.vue'
 import UserIcon from '../../assets/icons/UserIcon.vue'
 import PlusIcon from '../../assets/icons/PlusIcon.vue'
 import InfiniteLoading from 'v3-infinite-loading'
 import 'v3-infinite-loading/lib/style.css'
-import { toRefs } from 'vue'
+import { onMounted, ref, toRefs } from 'vue'
 import moment from 'moment'
+import { parseJwt } from '../../mixins/utils.js'
 import { useRouter } from 'vue-router'
 import { usePatientStore } from '../../store/patient.store'
 import { useModalStore } from '../../store/modal.store'
 import { useDropStore } from '../../store/drop.store'
 
 const router = useRouter()
+const payload = ref({})
 
 const props = defineProps({
   patients: { type: Array, required: true },
 })
 
 const { patients } = toRefs(props)
+
+const navigationGuard = (access) => {
+  return access.includes(payload.value?.role)
+}
 
 const addVisitThisPatient = (patient) => {
   useDropStore().setSelectPatientOption(patient)
@@ -29,6 +34,10 @@ const deletePatient = (selectedPatient) => {
   useModalStore().openDeleteAlertModal()
   usePatientStore().setSelectedPatient(selectedPatient)
 }
+
+onMounted(() => {
+  payload.value = parseJwt()
+})
 </script>
 <template>
   <tr class="border-y border-gray-200 hover:bg-gray-100 text-lg font-medium" v-for="(patient, idx) in patients" :key="idx">
@@ -53,13 +62,10 @@ const deletePatient = (selectedPatient) => {
     </td>
     <td v-motion-pop class="py-2 px-4 text-center">
       <div class="flex item-center justify-center">
-        <!-- <div v-if="router.currentRoute?.value?.path === '/patients'" class="w-4 mr-2 transform text-blue-500 hover:text-purple-500 hover:scale-110 cursor-pointer">
-          <EditIcon class="w-6 h-6" />
-        </div> -->
         <div v-if="router.currentRoute?.value?.path === '/patients'" @click="addVisitThisPatient(patient)" class="w-4 mr-2 transform text-blue-500 hover:text-purple-500 hover:scale-110 cursor-pointer">
           <PlusIcon class="w-6 h-6" />
         </div>
-        <div v-if="router.currentRoute?.value?.path === '/patients'" @click="deletePatient(patient)" class="w-4 mr-3 transform text-red-500 hover:text-red-600 hover:scale-110 cursor-pointer">
+        <div v-if="router.currentRoute?.value?.path === '/patients' && navigationGuard(['super_manager', 'tech_admin'])" @click="deletePatient(patient)" class="w-4 mr-3 transform text-red-500 hover:text-red-600 hover:scale-110 cursor-pointer">
           <TrashIcon class="w-6 h-6" />
         </div>
       </div>
