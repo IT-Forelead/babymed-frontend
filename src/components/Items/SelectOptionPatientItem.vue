96a5a9a7bb0593cbe78f55e224f72b9909d6a9c0
surojiddin <isurojiddin@gmail.com>
2024-03-13 10:57:04 +0500

    small change

---
+++ src/components/Items/SelectOptionPatientItem.vue
@@ -0,0 +1,20 @@
+<script setup>
+import InfiniteLoading from 'v3-infinite-loading'
+import 'v3-infinite-loading/lib/style.css'
+import { toRefs } from 'vue'
+
+const props = defineProps({
+  patients: { type: Array, required: true },
+})
+
+const { patients } = toRefs(props)
+</script>
+
+<template>
+  <div class="hover:bg-gray-200 cursor-pointer p-2 rounded-lg capitalize" v-for="(patient, idx) in patients" :key="idx">
+    {{ patient?.patient?.firstname + ' ' + patient?.patient?.lastname }}
+  </div>
+  <div class="flex items-center justify-center w-full p-2 rounded-lg">
+    <InfiniteLoading v-bind="$attrs" />
+  </div>
+</template>
